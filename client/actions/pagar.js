3e2b87973bbd655812e0f246dd7cf05907e27137
anderson <andfariasilva@gmail.com>
2016-03-02 18:48:03 -0300

    primeira versao

--- client/actions/pagar.js
+++ client/actions/pagar.js
@@ -1,32 +1,26 @@
+function pagar(valor) {
+	valor = valor * 10;
+	var checkout = new PagarMeCheckout.Checkout({"encryption_key":Meteor.settings.public.pagarmeKey_criptografia, success: function(data) {
+		if(data && data.token) {
+			Meteor.loginWithInstagram(function (err, res) {
+				if (err) {
+					console.log("login falhou");
+				}
+			});
+		}
+	}});
+
+	var params = {"customerData":false, "amount":valor*100, "createToken": "true", "paymentMethods": "credit_card" };
+	checkout.open(params);
+}
 Template.Pagar.events({
 	"click .bronze": function() {
-		Meteor.call("pagar", "bronze", function(erro, result) {
-			if(erro) {
-				showMensagemErro();
-			}
-			else {
-				showMensagemSucesso();
-			}
-		});
+		pagar(Meteor.settings.public.valorBronze);
 	},
 	"click .prata": function() {
-		Meteor.call("pagar", "prata", function(erro, result) {
-			if(erro) {
-				showMensagemErro();
-			}
-			else {
-				showMensagemSucesso();
-			}
-		});
+		pagar(Meteor.settings.public.valorPrata);
 	},
 	"click .ouro": function() {
-		Meteor.call("pagar", "ouro", function(erro, result) {
-			if(erro) {
-				showMensagemErro();
-			}
-			else {
-				showMensagemSucesso();
-			}
-		});
+		pagar(Meteor.settings.public.valorOuro);
 	},
 });
--- server/metodos/metodos.js
+++ server/metodos/metodos.js
@@ -1,29 +1,18 @@
 Meteor.methods({
-	"pagar": function(plano, cardHash) {
-		var valor = "";
-		if(plano == "bronze") {
-			valor = Meteor.settings.private.valorBronze * 100
+	associarTokenUsuario: function(data, valor) {
+		if(Meteor.userId()) {
+			HTTP.post("https://api.pagar.me/1/transactions/" + data.token + "/capture", {data: {api_key: Meteor.settings.private.pagarmeApiKey, amount: valor}},
+			function(err, result) {
+				if(err) {
+					throw new Meteor.Error("erroPagar", "Não conseguimos identificar o pagamento.");
+				}
+				else {
+					console.log(result);
+				}
+			});
 		}
-		else if(plano == "prata") {
-			valor = Meteor.settings.private.valorPrata * 100
+		else {
+			throw new Meteor.Error("erroLogar", "Você não está logado. Não podemos confirmar o valor pago.");
 		}
-		else if(plano == "ouro") {
-			valor = Meteor.settings.private.valorOuro * 100
-		}
-
-		HTTP.post("https://api.pagar.me/1/transactions", 
-			{data: {api_key: Meteor.settings.private.pagarmeApiKey, amount: valor, card_hash: cardHash, metadata: {'app': 'instawork'}}}, 
-		function(err, result) 
-		{
-			if(err) {
-				throw new Meteor.Error("erroPagar", "Erro ao efetuar o pagamento.");
-			}
-			Meteor.loginWithInstagram(function (err, res) {
-			    if (err !== undefined)
-					console.log('sucess ' + res)
-				else
-					console.log('login failed ' + err)
-		    });
-		});
 	}
 });
